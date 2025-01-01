2e336c2a6a9836034b4518378c850240ea98acbb
mdraselmiah95 <russelroy15@gmail.com>
2025-01-01 23:22:33 +0600

    FEAT: Learning C++

---
+++ Basic-lesson-c++/Chapter-01/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    //This is test comment and first program in c++
+    /*
+    This is multi line comment
+    */
+    std::cout << "i Like Egg." << "\n";
+    std::cout << "I like as this is Really good" << "\n";
+    return 0;
+}
+
