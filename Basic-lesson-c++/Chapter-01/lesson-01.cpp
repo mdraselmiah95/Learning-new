ad27ad423a5f2a40c1e39336ec1c554ee28025cc
mdraselmiah95 <russelroy15@gmail.com>
2025-01-01 23:32:56 +0600

    FEAT: Learning C++

---
+++ Basic-lesson-c++/Chapter-01/lesson-01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    // int x; declaration of variable
+   // x = 5; assignment of value to variable
+    int x = 5;
+    int y = 10;
+
+    std::cout << x << "\n";
+    std::cout << y<< "\n";
+    return 0;
+}
