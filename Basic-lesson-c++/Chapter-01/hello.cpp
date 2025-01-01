7e96e22cdd91f9269c7983de28affb5a7bba397c
mdraselmiah95 <russelroy15@gmail.com>
2025-01-01 23:42:57 +0600

    FEAT: Learning C++

--- Basic-lesson-c++/Chapter-01/hello.cpp
+++ Basic-lesson-c++/Chapter-01/hello.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+/**
+ *  iossream is a library that provides input and output functionality in C++. basically it is used to take input from user and display output to the user. This is a header file that is used to include the input output stream library in the program.
+ * int main() is the main function where program execution begins.
+ * ? std::cout stands for standard character output stream. It is used to display output to the standard output device. In this case, it is the console. It is defined in the iostream library.
+ * return 0; statement is used to exit the program. The value 0 indicates that the program has terminated successfully. The execution of the program starts from the main function. When the return statement is executed, the program terminates.
+*/
+
+
 int main(){
 
     //This is test comment and first program in c++
--- Basic-lesson-c++/Chapter-01/lesson-01.cpp
+++ Basic-lesson-c++/Chapter-01/lesson-01.cpp
@@ -6,8 +6,10 @@ int main(){
    // x = 5; assignment of value to variable
     int x = 5;
     int y = 10;
+    int sum= x+y;
 
     std::cout << x << "\n";
     std::cout << y<< "\n";
+    std::cout << sum << "\n";
     return 0;
 }
