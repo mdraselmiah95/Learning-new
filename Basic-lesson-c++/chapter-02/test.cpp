5275c6c2fa1911f57ef693ce91a01eefaf4ed0c6
mdraselmiah95 <russelroy15@gmail.com>
2025-04-17 22:49:33 +0600

    New code in c++

--- Basic-lesson-c++/chapter-02/test.cpp
+++ Basic-lesson-c++/chapter-02/test.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
-#include <vector>  // Include vector header
-#include <string>
 
-using namespace std;
 
 int main()
 {
-    vector<string> cars = {"Volvo", "BMW", "Ford", "Mazda"};
-
-    for (const string& car : cars)  // Use a reference to avoid unnecessary copying
-    {
-        cout << car << "\n";
-    }
-
-    return 0;
+    auto result = (10 <=> 20) > 0;
+    std::cout << result << std::endl;
 }
