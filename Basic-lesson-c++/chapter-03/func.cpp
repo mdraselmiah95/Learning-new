7f08d2375d0d6ffc05dde70167598163d3a6f7a3
mdraselmiah95 <russelroy15@gmail.com>
2025-04-18 01:52:01 +0600

    New code in c++ function

--- Basic-lesson-c++/chapter-03/func.cpp
+++ Basic-lesson-c++/chapter-03/func.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
 
+int addNumber(int first_param, int second_param){
+    int result = first_param + second_param;
+    return result;
+}
+
 int main(){
 
-    int first_number{3};
+    int first_number{3}; // Statement 
     int second_number{7};
 
     std::cout << "First number : " << first_number << std::endl;
     std::cout << "Second number : " << second_number << std::endl;
+
+    int sum = first_number + second_number;
+    std::cout << "Sum : " << sum << std::endl;
+
+    sum = addNumber(11, 77);
+    std::cout << "Fuck Sum : " << sum << std::endl;
 
     return 0;
 }
